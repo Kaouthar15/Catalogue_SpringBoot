032e7b4e9b6a0dfa7cfa183e46a271931e36448a
kaoutar <elazamikaoutar@gmail.com>
2024-11-13 19:43:46 +0100

    category service done

--- src/main/java/com/ntt/Catalogue/services/CategoryService.java
+++ src/main/java/com/ntt/Catalogue/services/CategoryService.java
@@ -1,5 +1,38 @@
 package com.ntt.Catalogue.services;
 
-public class CategoryService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.ntt.Catalogue.models.Category;
+import com.ntt.Catalogue.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	private CategoryRepository categoryRepository;
+	
+	public CategoryService(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+	
+	public void addCategory(Category category) {
+		categoryRepository.save(category);
+	}
+	
+	public Category save(Category category) {
+		return categoryRepository.save(category);
+	}
+	
+	public List<Category> getAllCategories(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category getCategoryById(Long id) {
+		return categoryRepository.findById(id).get(); 
+	}
+	
+	public void delete(Long id) {
+		categoryRepository.deleteById(id); 
+	}
+	
 }
