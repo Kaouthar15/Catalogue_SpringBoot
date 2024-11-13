0ac59b963abe8bcc69a14dcab9a3b85a82beeabb
kaoutar <elazamikaoutar@gmail.com>
2024-11-13 19:33:36 +0100

    product model done

--- src/main/java/com/ntt/Catalogue/models/Product.java
+++ src/main/java/com/ntt/Catalogue/models/Product.java
@@ -1,5 +1,65 @@
 package com.ntt.Catalogue.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class Product {
 
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Double price;
+	private Long quantity;
+	private String photo;
+	
+	@ManyToOne
+    @JoinColumn(name = "id_category", nullable = false)
+    private Category category;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
 }
