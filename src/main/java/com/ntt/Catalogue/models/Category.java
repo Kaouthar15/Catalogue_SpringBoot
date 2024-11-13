01ac74a6a772d46ef6e461111389f435b9e1e38b
kaoutar <elazamikaoutar@gmail.com>
2024-11-13 19:29:44 +0100

    Model Category Done

--- src/main/java/com/ntt/Catalogue/models/Category.java
+++ src/main/java/com/ntt/Catalogue/models/Category.java
@@ -1,5 +1,47 @@
 package com.ntt.Catalogue.models;
 
-public class Category {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "categories")
+public class Category {
+	
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private String description;
+	
+	public Category() {
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String toString() {
+		return "Category : id = " + id + " name = " + name + " description = "+ description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
 }
