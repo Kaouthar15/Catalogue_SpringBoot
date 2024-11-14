f291e5863397c064dfb7c3fb55c89c57ba8fc359
kaoutar <elazamikaoutar@gmail.com>
2024-11-14 12:00:53 +0100

    adding links on index to products and category + changing on product model

--- src/main/java/com/ntt/Catalogue/models/Product.java
+++ src/main/java/com/ntt/Catalogue/models/Product.java
@@ -2,6 +2,7 @@ package com.ntt.Catalogue.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -12,7 +13,7 @@ import jakarta.persistence.Table;
 public class Product {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private Double price;
