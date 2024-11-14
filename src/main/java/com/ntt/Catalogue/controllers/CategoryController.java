80017da246b5853b9d2ab755ccbf0d1022807c9e
kaoutar <elazamikaoutar@gmail.com>
2024-11-14 10:25:46 +0100

    adding the method update Category to the controller

--- src/main/java/com/ntt/Catalogue/controllers/CategoryController.java
+++ src/main/java/com/ntt/Catalogue/controllers/CategoryController.java
@@ -63,7 +63,12 @@ public class CategoryController {
 		return "redirect:/category";
 	}
 	
-	
-	
-
+	@PostMapping(value = "/update")
+	public String updateCategory(@RequestParam Long id, Map<String,Object> model, final RedirectAttributes redirectAttributes) {
+		categoryService.update(id);
+		List<Category> allCategories = categoryService.getAllCategories();
+		model.put("listCategories", allCategories);
+		redirectAttributes.addFlashAttribute("message","Category Updated Successfully !");
+		return "redirect:/category";
+	}
 }
