1197114dd26838b65f9418065710c7709dcba5af
kaoutar <elazamikaoutar@gmail.com>
2024-11-15 09:10:40 +0100

    product service done

--- src/main/java/com/ntt/Catalogue/services/ProductService.java
+++ src/main/java/com/ntt/Catalogue/services/ProductService.java
@@ -1,5 +1,33 @@
 package com.ntt.Catalogue.services;
 
-public class ProductService {
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.ntt.Catalogue.models.Product;
+import com.ntt.Catalogue.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	private ProductRepository productRepository;
+	
+	public ProductService(ProductRepository productRepository)
+	{
+		this.productRepository = productRepository;
+	}
+	
+	public Product save(Product product) {
+		return productRepository.save(product);
+	}
+	
+	public List<Product> getAllProducts(){
+		return productRepository.findAll();
+	}
+	
+	public Product getProductById(Long id) {
+		return productRepository.findById(id).get();
+	}
+	
+	public void delete(Long id) {
+		productRepository.deleteById(id);
+	}
 }
